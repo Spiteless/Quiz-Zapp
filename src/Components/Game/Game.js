c1e47a08f61733e125bbe160253c288e1167af23
Trillium <spiteless@gmail.com>
2020-09-22 14:26:20 -0700

    working single function to render board

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { withRouter } from "react-router-dom";
 // import axios from 'axios';
 import Card from "./Card"
-import { useSpring, animated as a } from 'react-spring'
 
 const sixteenCards = [
   {
